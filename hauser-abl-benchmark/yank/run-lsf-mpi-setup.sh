802b409bed77312a99305f000ac30e4fdbe25a9b
John Chodera <john.chodera@choderalab.org>
2018-03-22 08:36:48 -0400

    Update scripts

--- hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
+++ hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
@@ -10,7 +10,8 @@
 #
 # nodes: number of nodes and GPU request
 # 12 GPU's spread over 3 nodes
-#BSUB -n 1 -R "rusage[mem=12] span[ptile=4]"
+##BSUB -n 8 -R "rusage[mem=12] span[ptile=4]"
+#BSUB -n 8 -R "rusage[mem=12]"
 #BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
 #
 # job name (default = name of script file)
