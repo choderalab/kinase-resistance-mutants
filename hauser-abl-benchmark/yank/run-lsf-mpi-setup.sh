350e70ab37ae6629d5c013d95d66c52780c2dbf6
John Chodera <john.chodera@choderalab.org>
2018-03-22 21:03:09 -0400

    Use SDF files for ligands instead of mol2 to avoid ligand residue names that start with a number.

--- hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
+++ hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
@@ -11,7 +11,7 @@
 # nodes: number of nodes and GPU request
 # 12 GPU's spread over 3 nodes
 ##BSUB -n 8 -R "rusage[mem=12] span[ptile=4]"
-#BSUB -n 8 -R "rusage[mem=12]"
+#BSUB -n 16 -R "rusage[mem=12]"
 #BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
 #
 # job name (default = name of script file)
