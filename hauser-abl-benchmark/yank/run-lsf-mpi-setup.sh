9fe537d8e7f6cee39b95b8e512d28f02921c183a
John Chodera <john.chodera@choderalab.org>
2018-03-23 20:18:00 -0400

    Update YANK SAMS input

--- hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
+++ hauser-abl-benchmark/yank/run-lsf-mpi-setup.sh
@@ -1,5 +1,5 @@
 #Walltime
-#BSUB -W 8:00
+#BSUB -W 16:00
 #
 # Set Output file
 #BSUB -o  resistance-setup.%J.log
@@ -11,7 +11,7 @@
 # nodes: number of nodes and GPU request
 # 12 GPU's spread over 3 nodes
 ##BSUB -n 8 -R "rusage[mem=12] span[ptile=4]"
-#BSUB -n 16 -R "rusage[mem=12]"
+#BSUB -n 4 -R "rusage[mem=12]"
 #BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
 #
 # job name (default = name of script file)
