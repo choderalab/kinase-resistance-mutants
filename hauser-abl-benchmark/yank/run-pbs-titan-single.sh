872c1ffc9b94f1697fbfd86cb5b7ce8622418f17
John Chodera <jchodera1@titan-batch1.ccs.ornl.gov>
2018-03-24 18:45:41 -0400

    Update single-replica submit script

--- hauser-abl-benchmark/yank/run-pbs-titan-single.sh
+++ hauser-abl-benchmark/yank/run-pbs-titan-single.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #    Begin PBS directives
 #PBS -A chm126
-#PBS -N yank-imatinib-setup
+#PBS -N resistance-setup
 #PBS -j oe
-#PBS -l walltime=1:00:00,nodes=1
+#PBS -l walltime=2:00:00,nodes=1
 #PBS -l gres=atlas1%atlas2
 #PBS -l feature=gpudefault
 #    End PBS directives and begin shell commands
@@ -13,6 +13,8 @@ export MINICONDA="$HOME/miniconda3"
 export PATH="$MINICONDA/bin:$PATH"
 export LD_LIBRARY_PATH=$MINICONDA/lib:$LD_LIBRARY_PATH
 cd /lustre/atlas/scratch/jchodera1/chm126/kinase-resistance-mutants/hauser-abl-benchmark/yank/
+module load cudatoolkit
+export OPENMM_CUDA_COMPILER=`which nvcc`
 # Run only one job to set things up
 export OE_LICENSE="/lustre/atlas/scratch/jchodera1/chm126/.openeye/oe_license.txt"
-aprun -n $PBS_NUM_NODES yank script --yaml=imatinib.yaml
+aprun -n $PBS_NUM_NODES yank script --yaml=allmuts-sams-titan.yaml
