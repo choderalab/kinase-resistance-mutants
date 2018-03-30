d418b8251d08861effc3997d9392dd55900b76c6
John Chodera <jchodera1@titan-ext3.ccs.ornl.gov>
2018-03-29 20:34:56 -0400

    Update scripts

--- hauser-abl-benchmark/yank/run-pbs-benchmark.sh
+++ hauser-abl-benchmark/yank/run-pbs-benchmark.sh
@@ -3,7 +3,7 @@
 #PBS -A chm126
 #PBS -N resistance
 #PBS -j oe
-#PBS -l walltime=1:00:00,nodes=200
+#PBS -l walltime=0:20:00,nodes=1
 #PBS -l gres=atlas1%atlas2
 #PBS -l feature=gpudefault
 #    End PBS directives and begin shell commands
@@ -23,5 +23,5 @@ module add cray-mpich
 # Run only one job to set things up
 export OE_LICENSE="/lustre/atlas/scratch/jchodera1/chm126/.openeye/oe_license.txt"
 #aprun -n $PBS_NUM_NODES yank script --yaml=allmuts-sams.yaml --jobid="ALPS_APP_PE" --njobs="PBS_NUM_NODES"
-aprun -n $PBS_NUM_NODES -N 1 -d 16 yank script --yaml=allmuts-sams.yaml
-#aprun -n $PBS_NUM_NODES python benchmark.py
+#aprun -n $PBS_NUM_NODES yank script --yaml=allmuts-sams.yaml
+aprun -n $PBS_NUM_NODES python benchmark.py
