7c5122d5566b105e45d79b35f6dbc8faf74ad9f5
steven-albanese <steven.albanese91@gmail.com>
2018-06-05 14:41:39 -0400

    checking in docking results

--- fda-approved/fda-approved.prj/.mmproj-admin/maestro115.cmd
+++ fda-approved/fda-approved.prj/.mmproj-admin/maestro115.cmd
@@ -3,7 +3,7 @@
 # Contents of this file will be overwritten when the #
 # project is closed.                                 #
 ######################################################
-prefer fitenhance=true fitenhancenear=29.5909 fitenhancefar=-29.5909
+prefer fitenhance=true fitenhancenear=10.8581 fitenhancefar=-10.8581
 ribbon display=ribbonsonly
 hbondcriteria display=false displayhbond=true displayhalogen=true displaysaltbridge=true displayaromatichbond=false distance=2.8 donorangle=120 acceptorangle=90 halogendistance=3.5 donorminimumangleasdonor=140 acceptorminimumangleasdonor=90 donorminimumangleasacceptor=120 acceptorminimumangleasacceptor=90 acceptormaximumangleasacceptor=170 saltbridgedistance=5 aromatichbonddistance_o=2.8 aromatichbonddistance_n=2.5 aromatichbonddonorminangle_o=90 aromatichbonddonorminangle_n=108 aromatichbonddonormaxangle_n=130 aromatichbondacceptorminangle=90
 displayhbondsmode  mode=ligandreceptor
@@ -17,5 +17,5 @@ displaypiinteractions display=true displaystacking=true displaycation=true
 displaypiinteractionsmode  mode=ligandreceptor
 piinteractionset2 (protein_near_ligand) or (water and within 5.0 ligand)
 piinteractionset1 (ligand) or (water and within 5.0 ligand)
-clip front=-163.251 back=-338.03 frontsurface=-163.251 backsurface=-338.03 leftsurface=27.1396 rightsurface=201.918 leftslopesurface=0 rightslopesurface=0 frontselect=-163.251 backselect=-338.03 boxoffset=0 objects=all
+clip front=-176.427 back=-218.506 frontsurface=-176.427 backsurface=-218.506 leftsurface=97.9926 rightsurface=140.071 leftslopesurface=0 rightslopesurface=0 frontselect=-176.427 backselect=-218.506 boxoffset=0 objects=all
 prefer annotationsvisible=true interactionsvisible=false measurementsvisible=true ribbonsvisible=true surfacesvisible=true
