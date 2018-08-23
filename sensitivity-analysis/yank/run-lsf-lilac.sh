3f372192d994bdf6af77159fc1a1fa5f33f77d60
Steven Albanese <albaness@lilac.hpc.private>
2018-08-23 15:24:14 -0400

    checking in changes to submit and yaml files

--- sensitivity-analysis/yank/run-lsf-lilac.sh
+++ sensitivity-analysis/yank/run-lsf-lilac.sh
@@ -11,11 +11,12 @@
 #
 # nodes: number of nodes and GPU request
 #BSUB -n 8
-#BSUB -gpu "num=4:j_exclusive=yes:mode=shared" -R "rusage[mem=12] span[ptile=4]"
+#BSUB -gpu "num=1:j_exclusive=yes:mode=shared" -R "rusage[mem=10] span[ptile=1]"
 ##BSUB -R "{rusage[mem=12,ngpus_physical=2] span[ptile=2]} || {rusage[mem=12,ngpus_physical=3] span[ptile=3]} || {rusage[mem=12,ngpus_physical=4] span[ptile=4]} || {rusage[mem=12,ngpus_physical=1] span[ptile=1]}"
 #
 # job name (default = name of script file)
 #BSUB -J "sensitivity-lilac"
 
+source activate yank 
 build_mpirun_configfile --hostfilepath hostfile.${LSB_JOBNAME} --configfilepath configfile.${LSB_JOBNAME} "python run_yank.py --yaml=${LSB_JOBNAME}.yaml"
 mpiexec.hydra -f hostfile.${LSB_JOBNAME} -configfile configfile.${LSB_JOBNAME}
